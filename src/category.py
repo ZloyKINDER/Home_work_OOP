54c6cf6cccf65ac371e54e91055b7c28329c59a0
Timur Gorashchenko <timyr98239718@mail.ru>
2025-10-04 14:51:03 +0300

    create Category and Products

--- src/category.py
+++ src/category.py
@@ -4,9 +4,15 @@ class Category:
     name: str
     description: str
     products: list
+    category_count = 0
+    product_count = 0
 
     def __init__(self, name, description, products):
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.products = products
+
+        Category.category_count += 1
+        Category.product_count = len(products) if products else 0
+
